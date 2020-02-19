c81fb9a79318b3c3644fbe98b50833e37768e5a4
Eyu-Tes <eyoabtesfaye10@gmail.com>
2020-02-19 21:24:42 +0300

    last commit

--- static/custom/script.js
+++ static/custom/script.js
@@ -1,3 +1,5 @@
+// ------------------------- Image Modal ------------------------- //
+
 var modal = document.querySelector(".modal");
 var modalImg = document.querySelector(".modal-content");
 var imgs = document.querySelectorAll('.gallery .container img'); // Must insert space b|n .gallery & .container
@@ -15,3 +17,25 @@ for (i = 0; i < imgs.length; i++) {
 modalImg.addEventListener('click', function() {
     modal.style.display = "none";
 })
+
+
+// ------------------------- Date ------------------------- //
+
+var currentYear = new Date().getFullYear();
+spanElement = document.querySelector("#print-year");
+yearNode = document.createTextNode(currentYear);
+spanElement.appendChild(yearNode);
+
+
+// ------------------------- Scroll to Top ------------------------- //
+
+window.onscroll = function() { scrollFunction() };
+
+function scrollFunction() {
+    // console.log(document.body);
+    if (document.body.scrollTop > 610 || document.documentElement.scrollTop > 610) {
+        document.querySelector(".scroll-to-top").style.display = "block";
+    } else {
+        document.querySelector(".scroll-to-top").style.display = "none";
+    }
+}
